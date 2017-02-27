4e2d2a208d4e8ee5deb9714673cd7ee5cdeb51e2
ailov <ailov@naver.com>
2017-02-27 14:18:54 +0900

    ailov

--- mikionpub/Practice/module.querystring.js
+++
@@ -1,5 +0,0 @@
-var url = require('url');
-var querystring = require('querystring');
-
-var parsedObject = url.parse('http://hanbit.co.kr/store/books/look.php?p_code=B4250257160');
-console.log(querystring.parse(parsedObject.query));
--- mikionpub/Practice/module.url.js
+++
@@ -1,4 +0,0 @@
-var url = require('url');
-
-var parsedObject = url.parse('http://hanbit.co.kr/store/books/look.php?p_code=B4250257160');
-console.log(parsedObject);
